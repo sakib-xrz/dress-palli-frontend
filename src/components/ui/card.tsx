10ba94b021f9e9e95fb929345d5d28da63da6e84
sakib-xrz <sakibxrz21@gmail.com>
2026-02-11 15:57:24 +0600

    Refactor Card component styles: adjust padding in CardContent and CardFooter for improved layout consistency.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -65,7 +65,7 @@ function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-content"
-      className={cn("px-6", className)}
+      className={cn("px-4", className)}
       {...props}
     />
   );
@@ -75,7 +75,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+      className={cn("flex items-center px-4 [.border-t]:pt-4", className)}
       {...props}
     />
   );
