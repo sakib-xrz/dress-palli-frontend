41f603725b688a6d908e834c0660ebbc1d64c640
sakib-xrz <sakibxrz21@gmail.com>
2026-02-13 20:57:28 +0600

    Remove Home component: delete page.tsx file which contained a redirect to the login page.

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "./_components/footer";
+import Navbar from "./_components/navbar";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
